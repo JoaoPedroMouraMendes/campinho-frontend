88104caabc0cdf8ce29e08773480c5286f9261ea
JoaoPedroMouraMendes <joaopedromouramendes1@gmail.com>
2025-04-15 21:07:36 -0300

    feat: troca o favicon

---
+++ src/app/components/home-background/home-background.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-background',
+  imports: [],
+  templateUrl: './home-background.component.html',
+  styleUrl: './home-background.component.scss'
+})
+export class HomeBackgroundComponent {
+
+}
---
+++ src/app/components/reservation-card/reservation-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-card',
+  imports: [],
+  templateUrl: './reservation-card.component.html',
+  styleUrl: './reservation-card.component.scss'
+})
+export class ReservationCardComponent {
+
+}
