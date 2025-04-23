05c75a5690a559a716a94a65eed88d805d5e3b42
JoaoPedroMouraMendes <joaopedromouramendes1@gmail.com>
2025-04-23 16:59:32 -0300

    feat: adiciona o componente campinho-info a página inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeBackgroundComponent } from './components/home-background/home-background.component';
+import { CampinhoInfoComponent } from './components/campinho-info/campinho-info.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeBackgroundComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeBackgroundComponent, CampinhoInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
