b653ce1c145ab10c2680a32af9197fba23b77fcb
JoaoPedroMouraMendes <joaopedromouramendes1@gmail.com>
2025-04-24 21:17:31 -0300

    feat: adiciona card de serviços

---
+++ src/app/components/service-card/service-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-service-card',
+  imports: [],
+  templateUrl: './service-card.component.html',
+  styleUrl: './service-card.component.scss'
+})
+export class ServiceCardComponent {
+  @Input() title!: string;
+  @Input() text!: string;
+  @Input() imgPath!: string;
+  @Input() imgAlt!: string;
+}
