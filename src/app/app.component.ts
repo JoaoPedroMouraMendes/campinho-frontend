108c2a5398f783f33ef057025a37844ca31c5dc8
JoaoPedroMouraMendes <joaopedromouramendes1@gmail.com>
2025-04-25 21:58:26 -0300

    feat: adiciona o componente services-section a página inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeBackgroundComponent } from './components/home-background/home-background.component';
 import { CampinhoInfoComponent } from './components/campinho-info/campinho-info.component';
+import { ServicesSectionComponent } from './components/services-section/services-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeBackgroundComponent, CampinhoInfoComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeBackgroundComponent, CampinhoInfoComponent, ServicesSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
