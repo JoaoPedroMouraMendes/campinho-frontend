c07d0607b29daf3b5f6f84cc13a27bce6456f4db
JoaoPedroMouraMendes <joaopedromouramendes1@gmail.com>
2025-04-25 21:57:32 -0300

    feat: adiciona um component de sessão de cards de serviços

---
+++ src/app/components/services-section/services-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ServiceCardComponent } from '../service-card/service-card.component';
+
+@Component({
+  selector: 'app-services-section',
+  imports: [ServiceCardComponent],
+  templateUrl: './services-section.component.html',
+  styleUrl: './services-section.component.scss'
+})
+export class ServicesSectionComponent {
+
+}
