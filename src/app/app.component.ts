fb3d08facd9a4e6bf3782b26e1906e4c10c30e7e
JoaoPedroMouraMendes <joaopedromouramendes1@gmail.com>
2025-05-05 20:15:01 -0300

    feat: adiciona elementos a página inicial do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeBackgroundComponent } from './components/home-background/home-background.component';
 import { CampinhoInfoComponent } from './components/campinho-info/campinho-info.component';
 import { ServicesSectionComponent } from './components/services-section/services-section.component';
+import { LocationSectionComponent } from './components/location-section/location-section.component';
+import { PricingSectionComponent } from './components/pricing-section/pricing-section.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeBackgroundComponent, CampinhoInfoComponent, ServicesSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeBackgroundComponent, CampinhoInfoComponent,
+    ServicesSectionComponent, LocationSectionComponent, PricingSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
